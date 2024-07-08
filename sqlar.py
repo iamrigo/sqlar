04f2f114356e67c96f1f59feb71fe42c9787f396
Rigo <174382658+iamrigo@users.noreply.github.com>
2024-07-08 04:41:54 +0330

    Update sqlar.py

--- sqlar.py
+++ sqlar.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
+from aiogram.enums import ParseMode
+
 from tools.language.handler import MESSAGE
 from tools.db import db
 from tools.config import BOT_TOKEN, ADMIN_CHATID
@@ -25,12 +27,19 @@ async def handle_sql_query(message: types.Message, bot: Bot):
         await db.connect()
         query = message.text
         results = await db.execute_query(query)
-        response = "\n".join(str(row) for row in results)
-        if not response:
+
+        num_results = len(results)
+        if num_results == 0:
             response = await MESSAGE('SUCCESS_NO_MESSAGE')
-        await message.reply(response)
+            await message.reply(response)
+        else:
+            chunks = [results[i:i + 5] for i in range(0, num_results, 5)]
+            for chunk in chunks:
+                response = "\n".join(str(row) for row in chunk)
+                await message.reply(response)
+
     except Exception as e:
-        await message.reply(f"{await MESSAGE('ERROR')}\n\n{str(e)}")
+        await message.reply(f"<b>{await MESSAGE('ERROR')}</b>\n\n<pre>{str(e)}</pre>", parse_mode=ParseMode.HTML)
     finally:
         await db.disconnect()
 
