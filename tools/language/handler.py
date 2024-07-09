92f2fc356751b5b0a4ed5c9448f8022f0c1a84df
Rigo <174382658+iamrigo@users.noreply.github.com>
2024-07-09 04:07:02 +0330

    Update handler.py

--- tools/language/handler.py
+++ tools/language/handler.py
@@ -1,7 +1,8 @@
+import os
 import json
 from tools.config import LANGUAGE
 
-with open('tools/language/message.json', 'r', encoding='utf-8') as file:
+with open(os.path.join(os.path.dirname(__file__), 'message.json'), 'r', encoding='utf-8') as file:
     message = json.load(file)
 
 async def MESSAGE(key: str) -> str:
